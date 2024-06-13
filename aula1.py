b8873cedce8e8c38af33602e2788b9b26357062b
Viviane Jesus <vivianejesuscontato@gmail.com>
2024-06-13 20:39:09 -0300

    First Commit

---
+++ aula1.py
@@ -0,0 +1,9 @@
+nome = "PAULA MARTINS"
+mes = "JANEIRO"
+valor = "500,00"
+desconto = "10%"
+cupom = "PAULAÉ10"
+
+print("Olá, ", nome,", mes, "voce realizou uma compra no valor de ", "RS", valor," e ganhou um desconto de ",desconto, "de ", desconto, "em sua proxima compra." "Use o cupom , "cupom,")
+print("Olá, ", nome,". Em ", mes,"você realizou uma compra no valor de ","R$",valor," e ganhou um desconto de ",desconto," em sua próxima compra. Use o cupom ",cupom,".")
+print("Olá, ", nome,". Em ", mes,"você realizou uma compra no valor de ","R$",valor," e ganhou um desconto de ",desconto," em sua próxima compra. Use o cupom ",cupom,".")
